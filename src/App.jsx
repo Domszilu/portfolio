6a1f0d55567d27a036958081348b094dbb3b97a7
Dominik <m.dominik.andras@gmail.com>
2025-02-04 13:11:59 +0100

    set up global css, started navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes } from "react-router";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <Routes></Routes>
+        </>
+    );
+}
+
+export default App;
